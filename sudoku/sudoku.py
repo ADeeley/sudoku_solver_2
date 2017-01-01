1b7f440c1406ff3b33e0b6ef4275b6462a2d7056
adam deeley <ad.deeley@gmail.com>
2017-01-01 12:42:41 +0000

    Added unit tests for all functions and got the main sudoku program running.

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -17,25 +17,25 @@ bucket_locations = np.array([["b1", "b1", "b1", "b2", "b2", "b2", "b3", "b3", "b
                     ["b7", "b7", "b7", "b8", "b8", "b8", "b9", "b9", "b9"]])
     
 # test array of a complete and correct puzzle
-control = np.array([[9,8,3,4,5,7,6,2,1], 
-                     [1,6,7,2,8,9,3,5,4],
-                     [2,5,4,3,6,1,8,9,7],
-                     [8,1,5,7,9,4,2,3,6],
-                     [4,7,6,5,3,2,1,8,9],
-                     [3,2,9,6,1,8,4,7,5],
-                     [5,9,8,1,4,3,7,6,2],
-                     [6,4,2,8,7,5,9,1,3],
-                     [7,3,1,9,2,6,5,4,8]])
-              
-s = np.array([[0,0,0,4,5,7,0,2,1], 
-                     [1,6,0,0,0,0,0,5,4],
-                     [0,5,0,0,6,1,0,0,7],
-                     [0,0,5,7,9,4,0,3,6],
-                     [4,7,6,0,0,0,1,8,0],
-                     [0,2,0,6,1,0,4,7,5],
-                     [5,0,0,0,4,0,0,6,2],
-                     [6,0,2,0,0,5,0,0,3],
-                     [7,3,0,0,0,0,5,4,0]])              
+control = np.array([[8,2,9,4,3,1,6,5,7], 
+                [4,6,7,5,8,9,2,1,3],
+                [3,5,1,2,6,7,9,8,4],
+                [7,1,6,8,4,2,3,9,5],
+                [5,3,2,1,9,6,4,7,8],
+                [9,8,4,7,5,3,1,6,2],
+                [1,4,3,6,7,5,8,2,9],
+                [6,9,5,3,2,8,7,4,1],
+                [2,7,8,9,1,4,5,3,6]])   
+                                   
+s = np.array([[8,2,0,0,0,1,6,0,7], 
+            [0,6,7,5,0,9,2,0,0],
+            [3,0,0,0,6,0,9,0,0],
+            [0,1,0,0,0,0,3,9,5],
+            [0,0,2,0,0,0,4,0,0],
+            [9,8,4,0,0,0,0,6,0],
+            [0,0,3,0,7,0,0,0,9],
+            [0,0,5,3,0,8,7,4,0],
+            [2,0,8,9,0,0,0,3,6]])             
 #print(s)        
 
 
@@ -43,61 +43,65 @@ s = np.array([[0,0,0,4,5,7,0,2,1],
 # s = np.zeros(81)
 # s = s.reshape(9,9)
 
-def gen_buckets(s):
-    ''' Generates 9*(3*3) buckets to check values '''
+def gen_buckets(puzzle):
+    ''' Generates a dictionary with bucket names as the keys.
+        Each key has a list of the values present.
+        Should be updated when adding values to the puzzle
+    '''
+
     buckets = {}
 
-    a = s[0][0:3]
-    b = s[1][0:3]
-    c = s[2][0:3]
+    a = puzzle[0][0:3]
+    b = puzzle[1][0:3]
+    c = puzzle[2][0:3]
     bucket = np.concatenate([a,b,c])
     buckets["b1"] = bucket
 
-    a = s[0][3:6]
-    b = s[1][3:6]
-    c = s[2][3:6]
+    a = puzzle[0][3:6]
+    b = puzzle[1][3:6]
+    c = puzzle[2][3:6]
     bucket = np.concatenate([a,b,c])
     buckets["b2"] = bucket
 
-    a = s[0][6:9]
-    b = s[1][6:9]
-    c = s[2][6:9]
+    a = puzzle[0][6:9]
+    b = puzzle[1][6:9]
+    c = puzzle[2][6:9]
     bucket = np.concatenate([a,b,c])
     buckets["b3"] = bucket
 
-    a = s[3][0:3]
-    b = s[4][0:3]
-    c = s[5][0:3]
+    a = puzzle[3][0:3]
+    b = puzzle[4][0:3]
+    c = puzzle[5][0:3]
     bucket = np.concatenate([a,b,c])
     buckets["b4"] = bucket
 
-    a = s[3][3:6]
-    b = s[4][3:6]
-    c = s[5][3:6]
+    a = puzzle[3][3:6]
+    b = puzzle[4][3:6]
+    c = puzzle[5][3:6]
     bucket = np.concatenate([a,b,c])
     buckets["b5"] = bucket
 
-    a = s[3][6:9]
-    b = s[4][6:9]
-    c = s[5][6:9]
+    a = puzzle[3][6:9]
+    b = puzzle[4][6:9]
+    c = puzzle[5][6:9]
     bucket = np.concatenate([a,b,c])
     buckets["b6"] = bucket
 
-    a = s[6][0:3]
-    b = s[7][0:3]
-    c = s[8][0:3]
+    a = puzzle[6][0:3]
+    b = puzzle[7][0:3]
+    c = puzzle[8][0:3]
     bucket = np.concatenate([a,b,c])
     buckets["b7"] = bucket
 
-    a = s[6][3:6]
-    b = s[7][3:6]
-    c = s[8][3:6]
+    a = puzzle[6][3:6]
+    b = puzzle[7][3:6]
+    c = puzzle[8][3:6]
     bucket = np.concatenate([a,b,c])
     buckets["b8"] = bucket
 
-    a = s[6][6:9]
-    b = s[7][6:9]
-    c = s[8][6:9]
+    a = puzzle[6][6:9]
+    b = puzzle[7][6:9]
+    c = puzzle[8][6:9]
     bucket = np.concatenate([a,b,c])
     buckets["b9"] = bucket    
     
@@ -108,24 +112,24 @@ def gen_buckets(s):
 def gen_possibilities():
     ''' Assigns a list from 1-9 for each coordinate '''
     a = [1,2,3,4,5,6,7,8,9]
-    return [[a for x in range(9)] for x in range(9)]
+    poss =  np.array([[a for x in range(9)] for x in range(9)])
+    return poss
+    # if number already in puzzle- reduce the possibilities to []
+    
 
-
-def check_conflicts(val, n, buckets, puzzle):
+def is_conflicting(val, x, y, buckets, puzzle):
     '''Checks any conflicts within the row n[0] and the columnn[1]
     - val is an int of which we are checking for instances in the lists
     - n is a list of length 2
-    returns True if the val is in the lists, False otherwise
+    returns True if the val conflicts in the lists, False otherwise
     
     '''
-    assert type(n) == list, "n not list. n provided is of type %s" % type(n)
-    assert len(n) == 2, "n must be of length 2. n provided is of length %d" % len(n)
-    
-    bucket = bucket_locations[n[0]][n[1]]
-    numberToCheck = puzzle[n[0]][n[1]]
-    row = puzzle[n[0]]
-    col = puzzle[:, n[1]]
-    print(buckets[bucket], row, col)
+
+    bucket = bucket_locations[x][y]
+    numberToCheck = puzzle[x][y]
+    row = puzzle[x]
+    col = puzzle[:, y]
+    #print(buckets[bucket], row, col)
     return val in row or val in col or val in buckets[bucket]
     # add check bucket conflicts separately
     # convert n to two ints, c and r
@@ -134,57 +138,62 @@ def check_conflicts(val, n, buckets, puzzle):
 
 
 def trim_possibilities(x, y, poss, buckets, puzzle):
-    '''Works in a similar way to check_conflicts, but narrows down the possibility
+    '''Works in a similar way to is_conflicting, but narrows down the possibility
         space for each coordinate.
-        Calls check_conflicts for each possibility remaining in coordinate n.
-    - n is a list of length 2
+        Calls is_conflicting for each possibility remaining in coordinate n.
     '''
         # for every number which is not zero in the list, if it does not conflict, then add it to temp list
-    tempPoss = []
-    if puzzle[x][y] != 0 and poss[x][y] != []:
-        poss[x][y] = []
+
+    # if the number is already in the puzzle, set the possibilities to 0
+    if puzzle[x][y] != 0 and len(set(poss[x][y])) not in [1,2]: 
+        poss[x][y] = np.zeros(9, int)
         
-    for n in poss[x][y]:
-        # check if the number is an available possibility or if the value has already been assigned to the puzzle
-        if n == 0 or s[x][y] != 0:
+    for n in range(9):
+        # check if the number is an available slot in the puzzle or if the value has already been found
+        if poss[x][y][n] == 0 or puzzle[x][y] != 0:
             continue
-        result = check_conflicts(n, [x, y], buckets, puzzle)
-        print(result, x,y,n)
-        if result == False:
-          #  print("[", x, ",", y,"]", n)
-            tempPoss.append(n)
-    if tempPoss != []:
-        poss[x][y] = tempPoss
+        # if there are no conflicts, continue to next iteration
+        elif is_conflicting(poss[x][y][n], x, y, buckets, puzzle) == False:
+            continue
+        # otherwise, assign this value to 0 in order to ignore this possibility on the next pass
+        else:
+            poss[x][y][n] = 0
     
 
 def add_to_puzzle(puzzle, poss):
     '''Adds all len(1) possibility lists to the puzzle'''
     for x in range(9):        
         for y in range(9):
-            if len(poss[x][y]) == 1:
-                puzzle[x][y] = poss[x][y][0]
-                print(x,y, poss[x][y][0])
-                poss[x][y] = []
+            if len(set(poss[x][y])) == 2:
+                print("True", x, y)
+                puzzle[x][y] = sum(poss[x][y])
+
+               
+    return puzzle
     
 def trim_pass(puzzle, runs, poss, buckets):
     ''' Runs trim_possibilities runs amount of times '''
+    print_possibilities(poss)
     for run in range(runs):
+        buckets = gen_buckets(puzzle)
         for x in range(9):        
             for y in range(9):
                 
                 trim_possibilities(x,y, poss, buckets, puzzle)
-        add_to_puzzle(puzzle, poss)
+        print_possibilities(poss)
+        puzzle = add_to_puzzle(puzzle, poss)
 
+
+    return puzzle
+        
 def print_possibilities(poss):
     for x in range(9):        
         for y in range(9):
             print("row ", x, "col ", y, " ", poss[x][y])
 
 
-# ------------------------check_conflicts tests ----------------------
-# poss = gen_possibilities()
-# buckets = gen_buckets(s)
-# trim_pass(30)
-# print_possibilities()
-# print(s)            
-# print("Is equal to control:  \n", s == control)            
+#------------------------is_conflicting tests ----------------------
+print(s)
+s = trim_pass(s, 6, gen_possibilities(), gen_buckets(s))
+print(s)            
+print("Is equal to control:  \n", s == control)            
--- sudoku/sudoku_tests.py
+++ sudoku/sudoku_tests.py
@@ -1,64 +1,110 @@
 from sudoku import *
+import unittest
+import numpy as np
 
-# ---------------------- test 1 -----------------------
-def test_1():
-    solution = np.array([[9,8,3,4,5,7,6,2,1], 
-                                  [1,6,7,2,8,9,3,5,4],
-                                  [2,5,4,3,6,1,8,9,7],
-                                  [8,1,5,7,9,4,2,3,6],
-                                  [4,7,6,5,3,2,1,8,9],
-                                  [3,2,9,6,1,8,4,7,5],
-                                  [5,9,8,1,4,3,7,6,2],
-                                  [6,4,2,8,7,5,9,1,3],
-                                  [7,3,1,9,2,6,5,4,8]])
-                  
-    unsolved = np.array([[0,0,0,4,5,7,0,2,1], 
-                                   [1,6,0,0,0,0,0,5,4],
-                                   [0,5,0,0,6,1,0,0,7],
-                                   [0,0,5,7,9,4,0,3,6],
-                                   [4,7,6,0,0,0,1,8,0],
-                                   [0,2,0,6,1,0,4,7,5],
-                                   [5,0,0,0,4,0,0,6,2],
-                                   [6,0,2,0,0,5,0,0,3],
-                                   [7,3,0,0,0,0,5,4,0]])   
 
-    poss = gen_possibilities()
+
+class TestUnits(unittest.TestCase):
+    unsolved = np.array([[9,0,0,3,7,0,0,1,0],
+                        [6,2,0,0,9,0,0,0,0],
+                        [0,0,1,0,0,0,4,0,8],
+                        [0,0,0,5,3,0,0,0,0],
+                        [3,9,0,6,0,7,0,4,5],
+                        [0,0,0,0,1,4,0,0,0],
+                        [5,0,3,0,0,0,8,0,0],
+                        [0,0,0,0,4,0,0,5,3],
+                        [0,6,0,0,5,3,0,0,4]])   
+                
+    solution = np.array([[9,5,4,3,7,8,6,1,2],
+                    [6,2,8,4,9,1,5,3,7],
+                    [7,3,1,2,6,5,4,9,8],
+                    [4,1,6,5,3,2,7,8,9],
+                    [3,9,2,6,8,7,1,4,5],
+                    [8,7,5,9,1,4,3,2,6],
+                    [5,4,3,7,2,9,8,6,1],
+                    [2,8,7,1,4,6,9,5,3],
+                    [1,6,9,8,5,3,2,7,4]]) 
     buckets = gen_buckets(unsolved)
-    trim_pass(unsolved,1, poss, buckets)
-    #print_possibilities()
-    print(solution, "\n\n", unsolved)            
-    print("Is equal to control:  \n", unsolved == solution)      
-    print_possibilities(poss)
-
-     
-# ---------------------- test 2 -----------------------
-def test_2():
-    solution = np.array([[8,2,9,4,3,1,6,5,7], 
-                                   [4,6,7,5,8,9,2,1,3],
-                                   [3,5,1,2,6,7,9,8,4],
-                                   [7,1,6,8,4,2,3,9,5],
-                                   [5,3,2,1,9,6,4,7,8],
-                                   [9,8,4,7,5,3,1,6,2],
-                                   [1,4,3,6,7,5,8,2,9],
-                                   [6,9,5,3,2,8,7,4,1],
-                                   [2,7,8,9,1,4,5,3,6]])   
-                                   
-    unsolved = np.array([[8,2,0,0,0,1,6,0,7], 
-                                    [0,6,7,5,0,9,2,0,0],
-                                    [3,0,0,0,6,0,9,0,0],
-                                    [0,1,0,0,0,0,3,9,5],
-                                    [0,0,2,0,0,0,4,0,0],
-                                    [9,8,4,0,0,0,0,6,0],
-                                    [0,0,3,0,7,0,0,0,9],
-                                    [0,0,5,3,0,8,7,4,0],
-                                    [2,0,8,9,0,0,0,3,6]])
-          
-    poss = gen_possibilities()
-    buckets = gen_buckets(unsolved)
-    trim_pass(unsolved, 30, poss, buckets)
-    #print_possibilities()
-    print(unsolved)            
-    print("Is equal to control:  \n", unsolved == solution)      
-    print_possibilities(poss)
     
-test_2()
+    def test_gen_buckets(self):
+        
+        self.assertEqual(sum(self.buckets["b1"]), 18)
+        self.assertEqual(sum(self.buckets["b3"]), 13)
+        self.assertEqual(sum(self.buckets["b7"]), 14)
+        self.assertEqual(sum(self.buckets["b9"]), 20)
+
+    
+    def test_gen_possibilities(self):
+        a = [1,2,3,4,5,6,7,8,9]
+        poss =  np.array([[a for x in range(9)] for x in range(9)])
+        self.assertEqual(sum(poss[0][0]), sum(gen_possibilities()[0][0]))
+        self.assertEqual(sum(poss[8][8]), sum(gen_possibilities()[8][8]))
+        self.assertEqual(sum(poss[0][8]), sum(gen_possibilities()[0][8]))
+        self.assertEqual(sum(poss[8][0]), sum(gen_possibilities()[8][0]))
+     
+    def test_is_conflicting(self):
+        self.assertTrue(is_conflicting(3, 0,0 , self.buckets, self.unsolved))
+        self.assertFalse(is_conflicting(2, 0,8 , self.buckets, self.unsolved))
+        self.assertFalse(is_conflicting(2, 8,0 , self.buckets, self.unsolved))
+        self.assertFalse(is_conflicting(9, 8,8 , self.buckets, self.unsolved))
+
+    def test_trim_possibilities(self):
+        a = [1,2,3,4,5,6,7,8,9]
+
+        poss =  np.array([[a for x in range(9)] for x in range(9)])
+        x = trim_possibilities(0, 0, poss, gen_buckets(s), self.unsolved)
+        self.assertEqual(np.count_nonzero(poss[0][0]), 0)
+        
+        poss =  np.array([[a for x in range(9)] for x in range(9)])
+        x = trim_possibilities(1, 3, poss, gen_buckets(s), self.unsolved)
+        self.assertTrue(len(set(np.array(poss[1][3]))) == 2)
+
+    def test_add_to_puzzle(self):
+        poss = self.solution
+        add_to_puzzle(self.unsolved, poss)
+        
+        
+class general_tests():       
+    solution = np.array([[8,2,9,4,3,1,6,5,7], 
+                    [4,6,7,5,8,9,2,1,3],
+                    [3,5,1,2,6,7,9,8,4],
+                    [7,1,6,8,4,2,3,9,5],
+                    [5,3,2,1,9,6,4,7,8],
+                    [9,8,4,7,5,3,1,6,2],
+                    [1,4,3,6,7,5,8,2,9],
+                    [6,9,5,3,2,8,7,4,1],
+                    [2,7,8,9,1,4,5,3,6]])   
+                                       
+    unsolved = np.array([[8,2,0,0,0,1,6,0,7], 
+                        [0,6,7,5,0,9,2,0,0],
+                        [3,0,0,0,6,0,9,0,0],
+                        [0,1,0,0,0,0,3,9,5],
+                        [0,0,2,0,0,0,4,0,0],
+                        [9,8,4,0,0,0,0,6,0],
+                        [0,0,3,0,7,0,0,0,9],
+                        [0,0,5,3,0,8,7,4,0],
+                        [2,0,8,9,0,0,0,3,6]])
+    
+    def test_1(self, unsolved):
+  
+
+        poss = gen_possibilities()
+        buckets = gen_buckets(self.unsolved)
+        solved = trim_pass(self.unsolved, 10, poss, buckets)
+        #print_possibilities()
+        # print("Test 1\n", unsolved, "\n\n", solved)            
+        # print("Is equal to control:  \n", unsolved == solved)      
+
+
+         
+    def test_2(self):              
+        poss = gen_possibilities()
+        buckets = gen_buckets(self.unsolved)
+        solved = trim_pass(self.unsolved, 10, poss, buckets)
+        #print_possibilities()
+        # print("Test 2\n", unsolved, "\n\n", solved)            
+        # print("Is equal to control:  \n", unsolved == solved)      
+
+  
+if __name__ == '__main__':
+    unittest.main()
